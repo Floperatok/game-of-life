623d32a58d58ddc52e343f924fa6c12ce5a934b1
Floperatox <floperatox@gmail.com>
2025-05-15 22:23:53 +0200

    fix cells edit in negative chunks

--- sources/cell.c
+++ sources/cell.c
@@ -18,14 +18,22 @@ int get_cell(const unsigned char *data, int x, int y) {
 
 void mouse_cell_edit(data_t *data, int mouse_posx, int mouse_posy, char draging) {
 	int chunk_size = data->cam->cell_size * CHUNK_SIZE;
-	int posx_chunk = (-data->cam->x + mouse_posx) / chunk_size;
-	int posy_chunk = (-data->cam->y + mouse_posy) / chunk_size;
-	chunk_t *chunk = get_chunk(data->chunks, posx_chunk, posy_chunk);
+	int real_mouse_posx = -data->cam->x + mouse_posx;
+	int real_mouse_posy = -data->cam->y + mouse_posy;
+	int chunk_posx = real_mouse_posx / chunk_size;
+	int chunk_posy = real_mouse_posy / chunk_size;
+	if (real_mouse_posx < 0) {
+		chunk_posx--;
+	}
+	if (real_mouse_posy < 0) {
+		chunk_posy--;
+	}
+	chunk_t *chunk = get_chunk(data->chunks, chunk_posx, chunk_posy);
 	if (!chunk) {
 		return ;
 	}
-	int posx_cell = (-data->cam->x + mouse_posx) / data->cam->cell_size % CHUNK_SIZE;
-	int posy_cell = (-data->cam->y + mouse_posy) / data->cam->cell_size % CHUNK_SIZE;
+	int posx_cell = (real_mouse_posx - chunk_posx * CHUNK_SIZE * data->cam->cell_size) / data->cam->cell_size % CHUNK_SIZE;
+	int posy_cell = (real_mouse_posy - chunk_posy * CHUNK_SIZE * data->cam->cell_size) / data->cam->cell_size % CHUNK_SIZE;
 	if (!draging) {
 		data->inputs->creating_cells = !get_cell(chunk->cells, posx_cell, posy_cell);
 	}
